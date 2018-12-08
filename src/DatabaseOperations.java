12cb14bd75d790f1b10f4a48138bc73200a6d996
kubilayorcun <kubilay.orcun@icloud.com>
2018-12-08 23:36:11 +0300

    DatabaseOperations class created

---
+++ src/DatabaseOperations.java
@@ -0,0 +1,99 @@
+import java.sql.*;
+
+public class DatabaseOperations {
+
+    // Initialize global variables for global reference.
+    private ResultSet selectResultSet;
+    private Connection connection;
+
+    // Default constructor. Establish connection at the time of object declaration.
+    public DatabaseOperations(){
+        connectToDatabase();
+    }
+
+
+    // Creating the connection to database
+    public void connectToDatabase(){
+
+        String connectionUrl = "jdbc:sqlite:procatalog.db";
+
+        try {
+            connection = DriverManager.getConnection(connectionUrl);
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
+
+        // Dummy table creation for database functionality check.
+        // Does not duplicate table at each connection establish, NOT EXISTS keyword.
+        String createTable = "CREATE TABLE IF NOT EXISTS dummyTable (\n"
+                + " id INTEGER PRIMARY KEY,\n"
+                + " name text NOT NULL,\n"
+                + " surname text NOT NULL\n"
+                + ");";
+
+        try {
+            Statement createTableStt = connection.createStatement();
+            createTableStt.execute(createTable);
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
+    }
+
+    // Fetching passed parameter 'tableName' referenced table data to resultSet
+    public ResultSet selectFromTable(String tableName){
+
+        String selectQuery = "SELECT * FROM "+ tableName;
+
+        try {
+            Statement selectStatement = connection.createStatement();
+
+            selectResultSet = selectStatement.executeQuery(selectQuery);
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
+        return selectResultSet;
+    }
+
+    // Delete data dependant on id(primary_key) attribute of specific row.
+    public void deleteRowFromTable(String tableName, int id){
+
+        String deleteQuery = "DELETE FROM " +tableName+ " WHERE id = " + id;
+
+        try {
+
+            PreparedStatement deleteStatement = connection.prepareStatement(deleteQuery);
+            deleteStatement.executeUpdate();
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
+    }
+
+    // Deleting choosen column from table, will be added.
+    public void deleteColumnFromTable(String tableName, String columnName){
+
+    }
+
+
+    // Update table's data. (In our case, updates a collection's item's attribute.)
+    public void editDataFromTable(String tableName, int id, String columnChoice, String newValue){
+
+        // Ex: UPDATE books SET pageCount = 400 WHERE id = 37; (So the books table's 37th id'ed book's pagecount attribute will be updated to 400.)
+        String editDataQuery = "UPDATE " +tableName+ " SET " +columnChoice+ " = " +newValue+ " WHERE id = " +id;
+
+        try {
+            PreparedStatement editStatement = connection.prepareStatement(editDataQuery);
+            editStatement.executeUpdate();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+
+}
