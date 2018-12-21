a31e420850f2f305a7ab204e77a9416226a23411
kubilayorcun <kubilay.orcun@icloud.com>
2018-12-21 20:38:37 +0300

    Search functionality added

---
+++ src/CustomFrame.java
@@ -0,0 +1,10 @@
+import javax.swing.*;
+
+public class CustomFrame extends JFrame {
+    public CustomFrame(){
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setResizable(false);
+        setBounds(200,200,900,600);
+        //setVisible(true);
+    }
+}
